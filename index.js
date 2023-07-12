3163b90106582528d708814785d5ce45c3cd715e
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-12 20:33:52 +0100

    Add a function to build the proof that a particular leaf node exists within a merkle tree

--- index.js
+++ index.js
@@ -3,19 +3,56 @@ class MerkleTree {
         this.leaves = leaves;
         this.concat = concat;
     }
+
     getRoot(leaves = this.leaves) {
+        //top layer base case
         if (leaves.length === 1) {
             return leaves[0];
         }
+
+        // loop through each pair of nodes
         const combined = [];
-        for (let i = 0; i < leaves.length; i = i + 2) {
+        for (let i = 0; i < leaves.length; i += 2) {
+            // check if there's either one or both nodes, then save
             if (leaves[i + 1]) {
                 combined.push(this.concat(leaves[i], leaves[i + 1]));
             } else {
                 combined.push(leaves[i]);
             }
         }
+        // repeat recursively
         return this.getRoot(combined);
+    }
+
+    getProof(index, layer = this.leaves, proof = []) {
+        // top layer base case
+        if (layer.length === 1) return proof;
+
+        const newLayer = []
+        for (let i = 0; i < layer.length; i += 2) {
+            //store each side of the branch
+            let left = layer[i];
+            let right = layer[i + 1];
+
+            // if there's no right side then just take the left
+            if (!right) {
+                newLayer.push(left);
+            } else {
+                // store both for later
+                newLayer.push(this.concat(left, right));
+
+                // omit the node we are getting the proof for
+                if (i === index || i === index - 1) {
+                    let isLeft = !(index % 2);
+                    proof.push({
+                        data: isLeft ? right : left,
+                        left: !isLeft
+                    })
+                }
+            }
+        }
+        // repeat for the next layer up, recursively
+        return this.getProof(Math.floor(index / 2), newLayer, proof);
     }
 }
 
--- testMerkle.js
+++ testMerkle.js
@@ -1,42 +1,30 @@
+const {assert} = require("chai");
+const {hashProof, sha256, concatHash, concatLetters} = require('./testUtil');
 const MerkleTree = require('./index');
-const { assert } = require('chai');
 
-const concat = (a, b) => `Hash(${a} + ${b})`;
+describe('merkle proof', function() {
+    const leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    const root = 'eb100814abc896ab18bcf6c37b6550eeadeae0c312532286a4cf4be132ace526';
+    const hashTree = new MerkleTree(leaves.map(sha256), concatHash);
+    const lettersTree = new MerkleTree(leaves, concatLetters);
 
-describe('merkle', function () {
-    it('should handle the base case: [A]', function () {
-        const leaves = ['A'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "A");
-    });
-
-    it('should create a root from two leaves: [A,B]', function () {
-        const leaves = ['A', 'B'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "Hash(A + B)");
-    });
-
-    it('should create a root from four leaves: [A,B,C,D]', function () {
-        const leaves = ['A', 'B', 'C', 'D'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "Hash(Hash(A + B) + Hash(C + D))");
-    });
-
-    it('should create a root from three leaves: [A,B,C]', function() {
-        const leaves = ['A', 'B', 'C'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "Hash(Hash(A + B) + C)");
-    });
-
-    it('should create a root from five leaves: [A,B,C,D,E]', function () {
-        const leaves = ['A', 'B', 'C', 'D', 'E'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "Hash(Hash(Hash(A + B) + Hash(C + D)) + E)");
-    });
-
-    it('should create a root from seven leaves: [A,B,C,D,E,F,G]', function () {
-        const leaves = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-        const merkleTree = new MerkleTree(leaves, concat);
-        assert.equal(merkleTree.getRoot(), "Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + G))");
+    describe('for each leaf', function() {
+        leaves.forEach((leaf, i) => {
+            it(`should return a proof that calculates the root from leaf ${leaves[i]}`, function() {
+                const proof = hashTree.getProof(i);
+                const hashedProof = hashProof(leaf, proof).toString('hex');
+                if(hashedProof !== root) {
+                    const lettersProof = lettersTree.getProof(i);
+                    console.log(
+                        "The resulting hash of your proof is wrong. \n" +
+                        `We were expecting: ${root} \n` +
+                        `We received: ${hashedProof} \n` +
+                        `In ${leaves.join('')} Merkle tree, the proof of ${leaves[i]} you gave us is: \n` +
+                        `${JSON.stringify(lettersProof, null, 2)}`
+                    );
+                }
+                assert.equal(hashedProof, root);
+            });
+        });
     });
 });
