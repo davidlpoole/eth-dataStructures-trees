5055e3b2dd938446fe8f30e67dfdb02db95c2452
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-13 17:28:02 +0100

    Add a function `contains()` to the `Trie` class.
    This function takes a string word and returns true/false depending on whether the word is in our trie or not.

--- trie/Trie.js
+++ trie/Trie.js
@@ -4,20 +4,33 @@ class Trie {
     constructor() {
         this.root = new TrieNode(null);
     }
-
     insert(word) {
         let node = this.root;
-        [...word].forEach((letter, index) => {
-            if (!node.children[letter]) {
-                node = node.children[letter] = new TrieNode(letter);
+
+        for (let i = 0; i < word.length; i++) {
+            if (!node.children[word[i]]) {
+                node.children[word[i]] = new TrieNode(word[i]);
             }
-            else {
-                node = node.children[letter]
-            }
-            if (index === word.length - 1) {
+
+            node = node.children[word[i]];
+
+            if (i === word.length - 1) {
                 node.isWord = true;
             }
-        })
+        }
+    }
+    contains(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (node.children[word[i]]) {
+                node = node.children[word[i]];
+            } else {
+                return false;
+            }
+        }
+
+        return node.isWord;
     }
 }
 
