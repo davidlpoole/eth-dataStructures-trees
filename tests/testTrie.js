fe59e948d7cf90c82ddaa01c36f90224af937d51
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-13 14:55:14 +0100

    Add a method to insert a word to the Trie

--- tests/testTrie.js
+++ tests/testTrie.js
@@ -2,24 +2,36 @@ const {assert} = require('chai');
 const Trie = require('../trie/Trie');
 const TrieNode = require('../trie/TrieNode');
 
-describe('trie', () => {
-    it('should have a root trie node', () => {
-        const trie = new Trie();
-        assert(trie.root instanceof TrieNode, "expected trie to have a property `root` which is an instance of TrieNode");
-    });
-});
+describe('Trie', () => {
+    describe('with a single word', () => {
+        let trie;
+        beforeEach(() => {
+            trie = new Trie();
+            trie.insert('HEY');
+        });
 
-describe('TrieNode', () => {
-    it('should have store a key', () => {
-        const node = new TrieNode('a');
-        assert.equal(node.key, 'a', 'expected the constructor argument to be stored as the `key` property value');
-    });
-    it('should have store an object `children`', () => {
-        const node = new TrieNode();
-        assert.equal(typeof node.children, 'object', 'expected a property `children` to be an object on TrieNode');
-    });
-    it('should have store a property `isWord`', () => {
-        const node = new TrieNode();
-        assert.equal(node.isWord, false, 'expected a property `isWord` to be an object on TrieNode');
+        it('should connect the root to the first letter', () => {
+            const firstNode = trie.root.children['H'];
+            assert.equal(firstNode.key, 'H', 'expected the `key` of the first node to be `H`');
+            assert(firstNode.children['E'], 'expected the `children` of the first node to have a connection to the next letter');
+            assert.equal(firstNode.isWord, false, 'expected the `isWord` of the first node to be `false`');
+        });
+
+        it('should connect the root to the second letter', () => {
+            const firstNode = trie.root.children['H'];
+            const secondNode = firstNode.children['E'];
+            assert.equal(secondNode.key, 'E', 'expected the `key` of the first node to be `E`');
+            assert(secondNode.children['Y'], 'expected the `children` of the second node to have a connection to the next letter');
+            assert.equal(secondNode.isWord, false, 'expected the `isWord` of the second node to be `false`');
+        });
+
+        it('should connect the root to the third letter', () => {
+            const firstNode = trie.root.children['H'];
+            const secondNode = firstNode.children['E'];
+            const thirdNode = secondNode.children['Y'];
+            assert.equal(thirdNode.key, 'Y', 'expected the `key` of the first node to be `Y`');
+            assert.equal(Object.keys(thirdNode.children).length, 0, 'expected to have no `children` for the final node');
+            assert.equal(thirdNode.isWord, true, 'expected the `isWord` of the final node to be `true`');
+        });
     });
 });
--- trie/Trie.js
+++ trie/Trie.js
@@ -4,6 +4,16 @@ class Trie {
     constructor() {
         this.root = new TrieNode(null);
     }
+
+    insert(word) {
+        let node = this.root;
+        [...word].forEach((letter, index) => {
+            node = node.children[letter] = new TrieNode(letter);
+            if (index === word.length - 1) {
+                node.isWord = true;
+            }
+        })
+    }
 }
 
 module.exports = Trie;
